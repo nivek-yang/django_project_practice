6bb81e6ac1ea8b8e78ba3a15cf04fd219db142fa
KWY <nivekyang12014@gmail.com>
2025-04-14 15:22:42 +0800

    feat: Register Interview model in admin interface

--- interviews/admin.py
+++ interviews/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Interview
 
-# Register your models here.
+# 能客製化後台管理系統的類別，非必要
+class InterviewAdmin(admin.ModelAdmin):
+    pass
+
+# 註冊
+admin.site.register(Interview, InterviewAdmin)
