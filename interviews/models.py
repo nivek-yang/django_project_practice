dc2f13178717b280c401185eaff7ea9053e59d0e
KWY <nivekyang12014@gmail.com>
2025-04-14 14:51:19 +0800

    feat: Create Interview model and add migrations for database schema

--- interviews/models.py
+++ interviews/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Interview(models.Model):
+    company_name = models.CharField(max_length=100)
+    position = models.CharField(max_length=50)
+    interview_date = models.DateField(null=True)
+    review = models.TextField() # 專門拿來放很多字的欄位
+    rating = models.PositiveSmallIntegerField()
+    result = models.CharField(max_length=100, null=True)
+
+# - Table
+#     - 公司名稱 company_name
+#     - 職位 position
+#     - 面試日期 interview_date
+#     - 心得 review
+#     - 評分 rating
--- my_project/settings.py
+++ my_project/settings.py
@@ -31,15 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'myapp',
+    'myapp2',
+    'interviews',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myapp',
-    'myapp2',
-    'interviews',
 ]
 
 MIDDLEWARE = [
