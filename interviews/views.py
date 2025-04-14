b837915869809f4b87fa24adcbe28d5ab59a7dd2
KWY <nivekyang12014@gmail.com>
2025-04-14 15:45:20 +0800

    feat: Fetch and display interview list in interview index view

--- interviews/views.py
+++ interviews/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from .models import Interview
 
 # Create your views here.
 def index(req):
-    return render(req, "interviews/index.html")
+    # 抓面試列表
+    # 請 model 從資料庫調出資料
+    # select * from innterviews
+    interviews = Interview.objects.all()
+    return render(req, "interviews/index.html", {"interviews": interviews})
 
 def new(req):
     return render(req, "interviews/new.html")
