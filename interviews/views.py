513548fec97796f4b412356cd9c1f442973c8b45
KWY <nivekyang12014@gmail.com>
2025-04-14 11:38:30 +0800

    feat: Add new view and URL route for creating interviews

--- interviews/views.py
+++ interviews/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(req):
     return render(req, "interviews/home.html")
+
+def new(req):
+    return render(req, "interviews/new.html")
