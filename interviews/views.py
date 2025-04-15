b678c9172707a477f4b80255d72f1bf3a220d0e0
KWY <nivekyang12014@gmail.com>
2025-04-15 14:13:10 +0800

    feat: Improve index view to handle form submissions and redirect to show view after creating an interview

--- interviews/views.py
+++ interviews/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Interview
 from django.http import HttpResponse, Http404
+from django.urls import reverse
 
 # Create your views here.
 def index(req):
     if req.POST:
-        # 新增
+        # 處理表單提交：新增面試記錄
+        # 從 POST 請求中獲取表單數據
         company_name = req.POST['company_name']
         position = req.POST['position']
         interview_date = req.POST['interview_date']
@@ -13,6 +15,7 @@ def index(req):
         review = req.POST['review']
         result = req.POST['result']
 
+        # 在資料庫中創建新的面試記錄
         interview = Interview.objects.create(
             company_name=company_name,
             position=position,
@@ -22,12 +25,11 @@ def index(req):
             result=result,
         )
         
-        return HttpResponse("新增")
+        return redirect("interviews:show", id=interview.id)
     else:
-        # 抓面試列表
-        # 請 model 從資料庫調出資料
-        # select * from innterviews
-        interviews = Interview.objects.all()
+        # 顯示所有面試記錄列表
+        # 從資料庫中獲取所有面試記錄
+        interviews = Interview.objects.order_by("-id") # 依 id 反向排序
         return render(req, "interviews/index.html", {"interviews": interviews})
 
 def new(req):
@@ -36,9 +38,9 @@ def new(req):
 def show(req, id): # 參數要多加 id，從 urls 傳來的關鍵字引數
     # 抓資料
     # pk = primary key = 主鍵
-    interview = get_object_or_404(Interview, pk=id)
     # try:
     #     interview = Interview.objects.get(pk=id)
     # except:
     #     raise Http404("Interview does not exist")
+    interview = get_object_or_404(Interview, pk=id)
     return render(req, "interviews/show.html", {"interview": interview})
