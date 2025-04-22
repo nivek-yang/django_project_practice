272fdad5ae9c97e9dd38c50ca57c357418b728e2
KWY <nivekyang12014@gmail.com>
2025-04-22 15:38:04 +0800

    feat: Implement user sign-up form

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .forms import UserForm
 
 # Create your views here.
 def sign_up(req):
-    return render(req, "users/sign_up.html")
+    form = UserForm
+    return render(req, "users/sign_up.html", {"form": form})
